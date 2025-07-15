0251c33ce0c311e8062230f11979074eb1f08671
liorLahav <lior.lahav10@gmail.com>
2025-07-16 01:24:37 +0300

    Implement login component with dynamic form and validation

---
+++ src/app/auth/login/login.ts
@@ -0,0 +1,49 @@
+import { Component, Inject } from '@angular/core';
+import { InputField, submitResult } from '../dynamic-form/dynamic-form';
+import {NgForm } from '@angular/forms';
+import { User, USERS_SERVICE_TOKEN, UsersService } from '../../usersService';
+import { catchError, map, Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  constructor(@Inject(USERS_SERVICE_TOKEN) private users: UsersService){}
+  inputFields : InputField[]  = [{
+      title : "User name",
+      name: "username",
+      placeholder : "Username goes here",
+      required : true,
+      pattern: "^.{5,}$",
+  },{
+      title : "Password",
+      name:"password",
+      placeholder : "Dont foregt the password",
+      pattern : "^.{5,}$",
+      required : true
+  }]
+  
+  onSubmit = (f:NgForm) : Observable<submitResult> =>{
+    console.log(this.inputFields[0].value);
+    if(f.valid){
+      const username : string | undefined = this.inputFields[0].value;
+      const password : string | undefined = this.inputFields[1].value;
+      if (username && password){
+        return this.users.login(username,password).pipe(
+          map(data => data ? { success: true, message: "Logged in!" }
+            : { success: false, message: "Wrong credentials." }),
+          catchError(() => of({
+            success : false,
+            message : "Server problem."
+          })))
+      }
+    }
+    return of({
+      success: false,
+      message: "Form is invalid."
+    })
+  }
+}
