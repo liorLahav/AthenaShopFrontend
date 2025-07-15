d41a21f4a187739a0485c4eb6456d563401a0baa
liorLahav <lior.lahav10@gmail.com>
2025-07-16 01:26:15 +0300

    Added route support to login

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './auth/login/login';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path : 'login' , 
+  component : Login,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
