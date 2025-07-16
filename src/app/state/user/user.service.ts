fff0073e7ed5847b575ebc6dde86df13b0e1ca92
liorLahav <lior.lahav10@gmail.com>
2025-07-16 13:19:46 +0300

    Created akita and user store

---
+++ src/app/state/user/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { UserStore } from "./user.store";
+import { User } from "../../usersService";
+
+
+
+@Injectable({providedIn : 'root'})
+export class UserService{
+    constructor(private userstore : UserStore){}
+
+    updateisLoaded(isLoading : boolean){
+        this.userstore.setLoading(isLoading);
+    }
+    updateUser(user : User){
+        this.userstore.update({ user : user});
+        console.log(user);
+    }
+}
