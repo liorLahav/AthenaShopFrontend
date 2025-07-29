aba4bc7f40ee5caf21ffcac9774803619bb5ef50
liorLahav <lior.lahav10@gmail.com>
2025-07-30 01:33:02 +0300

    Add unAuthGuard and update routing to protect login and register routes

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,14 +4,17 @@ import { Login } from './auth/login';
 import { Register } from './auth/register';
 import { Main } from './main/main';
 import { authGuard } from './authGuard';
+import { unAuthGuard } from './unAuthGuard';
+
 
 const routes: Routes = [{
   path : 'login' , 
   component : Login,
-  
+  canActivate : [unAuthGuard]
 },{
   path : 'register',
   component : Register,
+  canActivate: [unAuthGuard]
 },{
   path : "",
   component : Main,
---
+++ src/app/unAuthGuard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+import { UserQuery } from "./state/user/user.query";
+
+@Injectable({
+    providedIn : "root"
+})
+export class unAuthGuard implements CanActivate{
+    constructor(private userQuery : UserQuery,private router : Router){}
+    canActivate(): boolean {
+        const isAuthed = this.userQuery.isAuthed;
+        if (isAuthed){
+            this.router.navigate(['/'])
+        }
+        return this.userQuery.isAuthed;
+    }
+}
