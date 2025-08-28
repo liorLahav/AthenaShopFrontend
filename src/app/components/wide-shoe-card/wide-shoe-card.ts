27f1e5a47e4db5958afdc7cd8460ffb2b05a824e
liorLahav <lior.lahav10@gmail.com>
2025-08-28 12:26:25 +0300

    Add wide shoe card component with styles and functionality

---
+++ src/app/components/wide-shoe-card/wide-shoe-card.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {getDisplayShoe, Shoe} from '../../services/shoesApi/shoesApiService'
+import { cartShoe } from '../../state/cart/cart.store';
+@Component({
+  selector: 'app-wide-shoe-card',
+  standalone: false,
+  templateUrl: './wide-shoe-card.html',
+  styleUrl: './wide-shoe-card.css'
+})
+export class WideShoeCard {
+  @Input({required: true}) shoe!: Shoe;
+  @Input({required : true}) size! : number;
+  @Output() remove = new EventEmitter<cartShoe>();
+
+  onXClick(){
+    this.remove.emit({
+      type: this.shoe,
+      size: this.size
+    })
+  }
+  get displayShoe(){
+    return getDisplayShoe(this.shoe);
+  }
+}
